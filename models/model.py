11bf62994d5d620c7218706b63a66f41b1fb6494
suojinhui <suojinhui@gmail.com>
2024-09-26 19:27:33 +0800

    test

--- models/model.py
+++ models/model.py
@@ -39,6 +39,7 @@ class CNNModel(nn.Module):
         input_data = input_data.expand(input_data.data.shape[0], 3, 28, 28)
         feature = self.feature(input_data)
         feature = feature.view(-1, 50 * 4 * 4)
+        # 梯度反转层
         reverse_feature = ReverseLayerF.apply(feature, alpha)
         class_output = self.class_classifier(feature)
         domain_output = self.domain_classifier(reverse_feature)
